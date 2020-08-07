95bf557ed3d13db44ae9381eab9ef0f491738464
utkarsh380 <patelutkarsh341@gmail.com>
2020-08-07 09:10:10 +0530

    files updated

--- index.php
+++ index.php
@@ -1,41 +1,72 @@
 <?php
+//index.php
 include 'functions.php';
-// Your PHP code here.
-if(isset($_GET['action'])) {
-	$contacts = simplexml_load_file('contact.xml');
-	$id = $_GET['id'];
-	$index = 0;
-	$i = 0;
-	foreach($contacts->contact as $contact){
-		if($contact['id']==$id){
-			$index = $i;
-			break;
-		}
-		$i++;
-	}
-	unset($contacts->contact[$index]);
-	file_put_contents('contact.xml', $contacts->asXML());
-}
-$contacts = simplexml_load_file('contact.xml');
-//echo 'Number of contacts: '.count($contacts);
-//echo '<br>List contact Information';
-// Home Page template below.
-
 ?>
-
 <?=template_header('Home')?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Import XML Data</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+ <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />-->
+  <link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+ </head>
+ <body>
+ 
+  <br />
+  <div class="container">
+   <div class="row">
+   
+    <br />
+    <div class="col-md-9" style="margin:0 auto; float:none;">
+     <span id="message"></span>
+     <form method="post" id="import_form" enctype="multipart/form-data">
+      <div class="form-group">
+       <label>Select XML File</label>
+       <input type="file" name="file" id="file" />
+      </div>
+      <br />
+      <div class="form-group">
+       <input type="submit" name="submit" id="submit" class="btn btn-info" value="Import" />
+      </div>
+     </form>
+    </div>
+   </div>
+  </div>
+ 
+ </body>
+</html>
+<script>
+$(document).ready(function(){
+ $('#import_form').on('submit', function(event){
+  event.preventDefault();
 
-<div class="content">
-	<h2>Home</h2>
-</div>
-<br>
+  $.ajax({
+   url:"import.php",
+   method:"POST",
+   data: new FormData(this),
+   contentType:false,
+   cache:false,
+   processData:false,
+   beforeSend:function(){
+    $('#submit').attr('disabled','disabled'),
+    $('#submit').val('Importing...');
+   },
+   success:function(data)
+   {
+    $('#message').html(data);
+    $('#import_form')[0].reset();
+    $('#submit').attr('disabled', false);
+    $('#submit').val('Import');
+   }
+  })
 
-<!--<a href='create.php'> 
-        <button class="btn"> 
-            ADD CONTACTS 
-        </button> 
-    </a>--> 
-<br>
+  setInterval(function(){
+   $('#message').html('');
+  }, 5000);
 
-
+ });
+});
+</script>
 <?=template_footer()?>
